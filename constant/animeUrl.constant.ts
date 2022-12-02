63f6394816ece3058dc72d307ff6d40090d225ce
Affan <affantaufiqur@gmail.com>
2022-12-02 23:40:06 +0700

    refactor card list header title to a component

--- constant/animeUrl.constant.ts
+++ constant/animeUrl.constant.ts
@@ -1,2 +1,2 @@
 export const getAnimeSeasonNowUrl = 'https://api.jikan.moe/v4/seasons/now'
-export const topAiringAnimeTv = 'https://api.jikan.moe/v4/top/anime?filter=airing'
+export const topAiringAnimeTv = 'https://api.jikan.moe/v4/top/anime?filter=airing&limit=10'
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import type { AnimeArrayDataType } from '../types/anime.types'
 import { getAnimeSeasonNowUrl, topAiringAnimeTv } from '../constant/animeUrl.constant'
 import Card from '../components/Card/Card.component'
+import CardHomeListHeaderText from '../components/Card/Card-home-list-header.component'
 
 export default function Home({ anime, topAiringAnime }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
@@ -21,17 +22,10 @@ export default function Home({ anime, topAiringAnime }: InferGetServerSidePropsT
       </Head>
       <main className="bg-black-shaft-900">
         <div className="sm:mx-auto mx-4 sm:container">
-          <section className="flex items-end justify-between py-6 text-white font-general-sans">
-            <h1 className="text-lg sm:text-2xl font-semibold text-white font-general-sans tracking-wide">
-              Fall 2022 Anime
-            </h1>
-            <Link
-              href={`/anime/season/${encodeURIComponent(anime.data[0].season)}`}
-              className="text-[0.75rem] tracking-wide text-port-gore-600 font-semibold hover:text-port-gore-500 transition-all duration-200"
-            >
-              VIEW MORE
-            </Link>
-          </section>
+          <CardHomeListHeaderText
+            title="Fall 2022 Anime"
+            href={`/anime/season/${encodeURIComponent(anime.data[0].season)}`}
+          />
           <div className="flex gap-5 flex-row overflow-x-scroll font-general-sans snap-x pb-10 sm:pb-1 sm:mx-0 no-scrollbar">
             {anime.data.map((item) => {
               return (
@@ -42,15 +36,10 @@ export default function Home({ anime, topAiringAnime }: InferGetServerSidePropsT
               )
             })}
           </div>
-          <section className="flex items-end justify-between py-6 text-white font-general-sans">
-            <h1 className="text-lg sm:text-2xl font-semibold text-white font-general-sans tracking-wide">Top airing</h1>
-            <Link
-              href={`/anime/season/${encodeURIComponent(anime.data[0].season)}`}
-              className="text-[0.75rem] tracking-wide text-port-gore-600 font-semibold hover:text-port-gore-500 transition-all duration-200"
-            >
-              VIEW MORE
-            </Link>
-          </section>
+          <CardHomeListHeaderText
+            title="Top airing"
+            href="/anime/top-airing"
+          />
           <div className="flex gap-5 flex-row overflow-x-scroll font-general-sans snap-x pb-10 sm:pb-1 sm:mx-0 no-scrollbar">
             {topAiringAnime.data.map((item) => {
               return (
@@ -76,8 +65,6 @@ export const getServerSideProps: GetServerSideProps<{
 
   const getTopAiringAnimeData: AnimeArrayDataType = await getTopAiringAnimeReq.json()
   const getAnimeSeasonNow: AnimeArrayDataType = await req.json()
-
-  console.log(getTopAiringAnimeData)
 
   return {
     props: {
