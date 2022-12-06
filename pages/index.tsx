5185fb86790fd53fb4d3ebb210340451b231c5f2
Affan <affantaufiqur@gmail.com>
2022-12-06 16:12:12 +0700

    fix anime by id page not refetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,25 @@ import { GetServerSideProps } from 'next'
 import Head from 'next/head'
 import Card from '../components/Card/Card.component'
 import CardHomeListHeaderText from '../components/Card/Card-home-list-header.component'
-import { QueryClient, dehydrate, useQuery } from '@tanstack/react-query'
+import { QueryClient, dehydrate, useQuery, useIsFetching } from '@tanstack/react-query'
 import { getTopAiringAnime, getAnimeFromSeason } from '../utils/getAnime.utils'
 import { getTopPusblishingManga } from '../utils/getManga.utils'
 import LoadingIndicator from '../components/LoadingIndicator.component'
 
 export default function Home() {
+  const isFetching = useIsFetching()
   const { data: SeasonalAnimeData } = useQuery({
     queryKey: ['fetch anime season'],
     queryFn: getAnimeFromSeason,
     staleTime: 300000, // 5 minutes
   })
 
-  const { data: TopAiringAnimeData, isFetching: isTopAiringAnimeDataFetching } = useQuery({
+  const { data: TopAiringAnimeData } = useQuery({
     queryKey: ['fetch top airing anime'],
     queryFn: getTopAiringAnime,
     staleTime: 300000, // 5 minutes
   })
-  const { data: TopPublishMangaData, isFetching: isMangaDataFetching } = useQuery({
+  const { data: TopPublishMangaData } = useQuery({
     queryKey: ['fetch top publishing manga'],
     queryFn: getTopPusblishingManga,
   })
@@ -43,21 +44,25 @@ export default function Home() {
             title="Fall 2022 Anime"
             href={`/anime/season/${encodeURIComponent(`${SeasonalAnimeData?.data[0]?.season}`)}`}
           />
-          <div className="no-scrollbar flex snap-x flex-row gap-5 overflow-x-scroll pb-10 font-general-sans sm:mx-0 sm:pb-1">
-            {SeasonalAnimeData?.data?.map((item) => {
-              return (
-                <Card
-                  item={item}
-                  key={item.mal_id}
-                />
-              )
-            })}
-          </div>
+          {isFetching ? (
+            <LoadingIndicator />
+          ) : (
+            <div className="no-scrollbar flex snap-x flex-row gap-5 overflow-x-scroll pb-10 font-general-sans sm:mx-0 sm:pb-1">
+              {SeasonalAnimeData?.data?.map((item) => {
+                return (
+                  <Card
+                    item={item}
+                    key={item.mal_id}
+                  />
+                )
+              })}
+            </div>
+          )}
           <CardHomeListHeaderText
             title="Top airing"
             href="/anime/top-airing"
           />
-          {isTopAiringAnimeDataFetching ? (
+          {isFetching ? (
             <LoadingIndicator />
           ) : (
             <div className="no-scrollbar flex snap-x flex-row gap-5 overflow-x-scroll pb-10 font-general-sans sm:mx-0 sm:pb-1">
@@ -75,7 +80,7 @@ export default function Home() {
             title="Top manga"
             href="/manga/top-manga"
           />
-          {isMangaDataFetching ? (
+          {isFetching ? (
             <LoadingIndicator />
           ) : (
             <div className="no-scrollbar flex snap-x flex-row gap-5 overflow-x-scroll pb-10 font-general-sans sm:mx-0 sm:pb-1">
