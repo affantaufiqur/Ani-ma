140bbb5eaf8bd1cd6db948acd9195417ca0ce720
Affan <affantaufiqur@gmail.com>
2022-12-06 20:45:31 +0700

    added japanese title alt, description, and metadata

--- pages/anime/[anime_id].tsx
+++ pages/anime/[anime_id].tsx
@@ -1,5 +1,6 @@
 import { useQuery, QueryClient, dehydrate } from '@tanstack/react-query'
 import type { GetServerSideProps, GetServerSidePropsContext, InferGetServerSidePropsType } from 'next'
+import Head from 'next/head'
 import Image from 'next/image'
 import { ParsedUrlQuery } from 'querystring'
 import LoadingIndicator from '../../components/LoadingIndicator.component'
@@ -30,31 +31,77 @@ export default function AnimePage({ params }: InferGetServerSidePropsType<typeof
   }
 
   return (
-    <main className="min-h-screen bg-black-shaft-900">
-      <div className="container mx-auto font-general-sans">
-        <section className="flex flex-row gap-5">
-          <Image
-            src={data?.data?.images.webp.large_image_url as string}
-            alt={data?.data?.title}
-            width={240}
-            height={340}
-            priority
-            className="h-auto w-auto"
-          />
-          <section className="flex flex-col gap-4">
-            <p className="text-2xl font-semibold text-white">{data?.data?.titles[0].title}</p>
-            <p className="text-md flex flex-col gap-2 font-normal text-black-shaft-400">
-              {data?.data?.titles.map((titleAlt) => {
-                return (
-                  <p key={titleAlt.type}>
-                    {titleAlt.type}: {titleAlt.title}
+    <main>
+      <Head>
+        <title>{data.data.titles[0].title} | Anima</title>
+        <meta
+          name="description"
+          content="Anime details"
+        />
+        <link
+          rel="icon"
+          href="/favicon.ico"
+        />
+      </Head>
+      <main className="min-h-screen bg-black-shaft-900">
+        <div className="container mx-auto font-general-sans">
+          <section className="flex flex-row gap-5">
+            <Image
+              src={data?.data?.images.webp.large_image_url as string}
+              alt={data?.data?.title}
+              width={240}
+              height={340}
+              priority
+              className="h-auto w-auto rounded-md"
+            />
+            <section className="max-w-3/4 flex flex-col justify-between">
+              <section className="flex flex-col gap-5">
+                <section>
+                  <h1 className="text-2xl font-semibold tracking-wide text-white">{data?.data?.titles[0].title}</h1>
+                  <h3 className="text-md font-normal text-black-shaft-400">
+                    {data?.data?.titles.map((titleAlt) => {
+                      return <p key={titleAlt.type}>{titleAlt.type === 'Japanese' ? titleAlt.title : ''}</p>
+                    })}
+                  </h3>
+                </section>
+                <section className="w-3/4">
+                  <p className="whitespace-normal break-words text-sm leading-loose tracking-wide text-black-shaft-100">
+                    {data.data.synopsis}
                   </p>
-                )
-              })}
-            </p>
+                </section>
+              </section>
+              <section>
+                <div className="inline-block rounded-full border-2 border-white p-4">
+                  <div className="flex flex-row gap-x-3 [&_p]:font-medium [&_p]:tracking-wide [&_p]:text-black-shaft-100 [&_span]:font-normal [&_span]:text-black-shaft-300">
+                    <p>
+                      <span>Type: </span> {data.data.type}
+                    </p>
+                    <p>
+                      <span>Status: </span> {data.data.status}
+                    </p>
+                    <p>
+                      <span>Rating: </span> {data.data.rating}
+                    </p>
+                    <p>
+                      <span>Episodes: </span>
+                      {data.data.episodes}
+                    </p>
+                    <p>
+                      <span>Year: </span> {data.data.year}
+                    </p>
+                    <p>
+                      <span>Season: </span> {data.data.season}
+                    </p>
+                    <p>
+                      <span>Genre: </span> {data.data.genres[0].name}
+                    </p>
+                  </div>
+                </div>
+              </section>
+            </section>
           </section>
-        </section>
-      </div>
+        </div>
+      </main>
     </main>
   )
 }
