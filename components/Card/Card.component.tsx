187ee1796aedab22092536f6df47fec172ba889a
Affan <affantaufiqur@gmail.com>
2022-12-06 10:24:25 +0700

    fix href not dynamic

--- components/Card/Card.component.tsx
+++ components/Card/Card.component.tsx
@@ -10,20 +10,20 @@ type itemType = {
 export default function Card({ item }: itemType) {
   return (
     <Link
-      href={`/anime/${encodeURIComponent(item.mal_id)}`}
+      href={`/${item?.genres[0]?.type}/${encodeURIComponent(item?.mal_id)}`}
       className="group flex shrink-0 basis-48 cursor-pointer flex-col gap-y-3"
       key={item.mal_id}
     >
       <Image
-        src={item.images.webp.image_url}
-        alt={item.title}
+        src={item?.images?.webp?.image_url}
+        alt={item?.titles[0]?.title}
         width={192}
         height={256}
         className="h-64 w-auto snap-start transition-all duration-100 hover:brightness-75"
         priority
       />
       <p className="break-words text-sm font-medium leading-tight tracking-wider text-black-shaft-200 transition-all duration-100 group-hover:block group-hover:text-port-gore-700">
-        {item.titles[0].title}
+        {item.titles[0].title.substring(0, 40)}
       </p>
     </Link>
   )
